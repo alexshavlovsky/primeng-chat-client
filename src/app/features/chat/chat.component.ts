db56051739b45183ce4fb9ddce7b966c88b8213c
alexshavlovsky <ctzn@tut.by>
2020-04-21 00:25:57 +0300

    enable a user nick modification in the main chat corner menu input box

--- src/app/features/chat/chat.component.ts
+++ src/app/features/chat/chat.component.ts
@@ -2,10 +2,10 @@ import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core
 import {UserPrincipalService} from '../../core/services/user-principal.service';
 import {Router} from '@angular/router';
 import {MenuItem} from 'primeng/api';
-import {Observable, Subscription} from 'rxjs';
+import {Observable, Subject, Subscription} from 'rxjs';
 import {WsService} from '../../core/services/ws.service';
 import {ChatSnapshotService} from '../../core/services/chat-snapshot.service';
-import {filter, map, tap} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, filter, map, tap} from 'rxjs/operators';
 import {UserPrincipal} from '../../core/models/user-principal.model';
 import {ChatClientModel} from '../../core/models/chat-client.model';
 import {ServerMessageModel} from '../../core/models/server-message.model';
@@ -21,6 +21,9 @@ export class ChatComponent implements OnInit, OnDestroy {
   messages: ServerMessageModel[] = [];
   subscription: Subscription;
   inputText = '';
+
+  nick: string;
+  nickChanged: Subject<string> = new Subject<string>();
 
   @ViewChild('messagesScroll') private msgScroll: ElementRef;
 
@@ -49,6 +52,16 @@ export class ChatComponent implements OnInit, OnDestroy {
       tap(m => {
         this.messages.push(m);
         setTimeout(() => this.scrollToBottom());
+      })
+    ).subscribe();
+    this.nick = this.principal.nick;
+    this.nickChanged.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      filter(nick => nick !== null && this.nick.length !== 0),
+      tap(nick => {
+        this.userPrincipalService.setPrincipal(nick);
+        this.ws.updateUserDetails();
       })
     ).subscribe();
   }
